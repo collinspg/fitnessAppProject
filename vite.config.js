18456cc66f6b06a96c2cc15358c7ad0f90cb7c8d
collinspg <collinsokolie90@gmail.com>
2025-04-10 01:42:51 +0100

    added partials to vite.config

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,8 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         dashboard: resolve(__dirname, "src/pages/dashboard.html"),
         nutrition: resolve(__dirname, "src/pages/nutrition.html"),
+        header: resolve(__dirname, "src/partials/header.html"),
+        footer: resolve(__dirname, "src/partials/footer.html"),
       
       },
     },
